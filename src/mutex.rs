746dcbb75f1da8dc39951badfdfb0c67cb9cc85e
Peter Atashian <retep998@gmail.com>
2019-04-19 05:27:32 -0400

    Make Mutex able to own data

--- src/mutex.rs
+++ src/mutex.rs
@@ -6,6 +6,8 @@
 
 use std::{
     marker::PhantomData,
+    mem::size_of,
+    ops::Deref,
     ptr::null_mut,
 };
 use winapi::{
@@ -16,32 +18,42 @@ use winapi::{
     um::{
         errhandlingapi::GetLastError,
         minwinbase::SECURITY_ATTRIBUTES,
-        processthreadsapi::GetCurrentProcess,
         synchapi::{CreateMutexW, OpenMutexW, ReleaseMutex, WaitForSingleObject},
         winbase::{INFINITE, WAIT_ABANDONED, WAIT_OBJECT_0},
-        winnt::{DUPLICATE_SAME_ACCESS, HANDLE, SYNCHRONIZE},
+        winnt::{HANDLE, SECURITY_DESCRIPTOR, SYNCHRONIZE},
     },
 };
 use error::Error;
 use handle::Handle;
 use wide::ToWide;
 
-pub struct Mutex(Handle);
-impl Mutex {
-    pub fn create(security_attributes: Option<&SECURITY_ATTRIBUTES>, name: &str) -> Result<Mutex, Error> {
+pub struct SecurityAttributes(SECURITY_ATTRIBUTES);
+impl SecurityAttributes {
+    pub unsafe fn from_raw(sd: *mut SECURITY_DESCRIPTOR) -> SecurityAttributes {
+        SecurityAttributes(SECURITY_ATTRIBUTES {
+            nLength: size_of::<SECURITY_ATTRIBUTES>() as u32,
+            lpSecurityDescriptor: sd as *mut _,
+            bInheritHandle: FALSE,
+        })
+    }
+}
+
+pub struct Mutex<T>(Handle, T);
+impl<T> Mutex<T> {
+    pub fn create(data: T, mut security_attributes: Option<SecurityAttributes>, name: &str) -> Result<Mutex<T>, Error> {
         unsafe {
             let handle = CreateMutexW(
-                security_attributes.map(|x| x as *const _ as *mut _).unwrap_or(null_mut()),
+                security_attributes.as_mut().map(|x| &mut x.0 as *mut _).unwrap_or(null_mut()),
                 0,
                 name.to_wide_null().as_ptr(),
             );
             if handle.is_null() {
                 return Error::last_result();
             }
-            Ok(Mutex(Handle::new(handle)))
+            Ok(Mutex(Handle::new(handle), data))
         }
     }
-    pub fn open(name: &str) -> Result<Mutex, Error> {
+    pub fn open(data: T, name: &str) -> Result<Mutex<T>, Error> {
         unsafe {
             let handle = OpenMutexW(
                 SYNCHRONIZE,
@@ -51,12 +63,12 @@ impl Mutex {
             if handle.is_null() {
                 return Error::last_result();
             }
-            Ok(Mutex(Handle::new(handle)))
+            Ok(Mutex(Handle::new(handle), data))
         }
     }
     /// The timeout is specified in milliseconds
     /// Specifying None for the timeout means to wait forever
-    pub fn wait<'a>(&'a self, timeout: Option<u32>) -> Result<MutexGuard<'a>, WaitError<'a>> {
+    pub fn wait<'a>(&'a self, timeout: Option<u32>) -> Result<MutexGuard<'a, T>, WaitError<'a, T>> {
         unsafe {
             match WaitForSingleObject(*self.0, timeout.unwrap_or(INFINITE)) {
                 WAIT_ABANDONED => Err(WaitError::Abandoned(MutexGuard::new(self))),
@@ -66,33 +78,39 @@ impl Mutex {
             }
         }
     }
-    pub fn try_clone(&self) -> Result<Mutex, Error> {
+    pub fn try_clone(&self) -> Result<Mutex<T>, Error> where T: Clone {
         unsafe {
             let handle = Handle::duplicate_from(*self.0)?;
-            Ok(Mutex(handle))
+            Ok(Mutex(handle, self.1.clone()))
         }
     }
 }
-unsafe impl Send for Mutex {}
-unsafe impl Sync for Mutex {}
-pub struct MutexGuard<'a>(HANDLE, PhantomData<&'a Mutex>);
-impl<'a> MutexGuard<'a> {
-    unsafe fn new(mutex: &'a Mutex) -> MutexGuard<'a> {
-        MutexGuard(*mutex.0, PhantomData)
+unsafe impl<T> Send for Mutex<T> where T: Send {}
+unsafe impl<T> Sync for Mutex<T> where T: Sync {}
+pub struct MutexGuard<'a, T>(&'a Mutex<T>, PhantomData<HANDLE>);
+impl<'a, T> MutexGuard<'a, T> {
+    unsafe fn new(mutex: &'a Mutex<T>) -> MutexGuard<'a, T> {
+        MutexGuard(mutex, PhantomData)
     }
 }
-impl<'a> Drop for MutexGuard<'a> {
+impl<'a, T> Drop for MutexGuard<'a, T> {
     fn drop(&mut self) {
         unsafe {
-            if ReleaseMutex(self.0) == 0 {
+            if ReleaseMutex(*(self.0).0) == 0 {
                 let err = GetLastError();
                 panic!("failed to call ReleaseMutex: {}", err);
             }
         }
     }
 }
-pub enum WaitError<'a> {
+impl<'a, T> Deref for MutexGuard<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &(self.0).1
+    }
+}
+pub enum WaitError<'a, T> {
     Timeout,
-    Abandoned(MutexGuard<'a>),
+    Abandoned(MutexGuard<'a, T>),
     Other(Error),
 }
